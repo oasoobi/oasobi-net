4eededc31b536ff753e8d8f543bf91e790f1ab3b
oasobi <oasobyy@gmail.com>
2024-10-17 07:10:33 +0000

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,9 +20,13 @@ import { useEffect, useState } from 'react'
 export const Header = () => {
     const {resolvedTheme} = useTheme();
     const [color, setColor] = useState<string>("");
+    const [isOpen, setIsOpen] = useState<boolean>(false);
     useEffect(() => {
         setColor(resolvedTheme == "light" ? "#000000" : "#ffffff");
-    }, [resolvedTheme])
+    }, [resolvedTheme]);
+    const toggleOpen = () => {
+        setIsOpen(!isOpen);
+    }
     return (
         <div>
             <NextTopLoader showSpinner={false} height={1.6} color={color} />
@@ -31,17 +35,21 @@ export const Header = () => {
                     おあそび
                 </Link>
                 <div className='flex gap-4 items-center select-none'>
-                    <Link href="/downloads" legacyBehavior passHref>
-                        Downloads
-                    </Link>
-                    <Link href="/tools" legacyBehavior passHref className='ml-2'>
-                        Tools
-                    </Link>
-                    <ModeToggle />
+                    <div className='flex items-center select-none'>
+                    <svg className="lg:hidden" onClick={() => {toggleOpen}} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="6 9 12 15 18 9"></polyline></svg>
+                        <div className='hidden lg:flex items-center gap-3 select-none'>
+                            <Link href="/downloads" legacyBehavior passHref className='hidden'>
+                                Downloads
+                            </Link>
+                            <Link href="/tools" legacyBehavior passHref className='ml-2'>
+                                Tools
+                            </Link>
+                        </div>
+                        <ModeToggle />
+                    </div>
                 </div>
             </div>
         </div>
-
     )
 }
 
